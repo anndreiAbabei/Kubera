07660334142ba2600c5aa3c0e0e747f76354683c
Ababei Andrei <AbabeiAndrei@outlook.com>
2021-01-31 23:14:12 +0200

    Fix asset

--- Kubera.Data/Entities/Asset.cs
+++ Kubera.Data/Entities/Asset.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using Kubera.General.Entities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Kubera.Data.Entities
 {
-    public class Asset : Currency
+    public class Asset : Entity, ISoftDeletable
     {
+        public virtual string Code { get; set; }
+
+        public virtual string Name { get; set; }
+
+        public virtual string Symbol { get; set; }
+
+        public virtual int Order { get; set; }
+
         public virtual DateTime CreatedAt { get; set; }
 
         public virtual Guid GroupId { get; set; }
@@ -12,6 +22,8 @@ namespace Kubera.Data.Entities
         public virtual string OwnerId { get; set; }
 
         public virtual string Icon { get; set; }
+
+        public virtual bool Deleted { get; set; }
 
         public virtual Group Group { get; set; }
 
