5cecc10ac6616fc991112a3446c6ff5e7817c410
Andrei Ababei <ababeiandrei94@gmail.com>
2021-02-14 21:00:25 +0200

    Added tests

---
+++ tests/Kubera.App.IntegrationTests/AlphaVantage/AlphaVantageServiceFactory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using Kubera.Business.Services;
+using Kubera.General.Services;
+
+namespace Kubera.App.IntegrationTests.AlphaVantage
+{
+    public class AlphaVantageServiceFactory : IntegrationTestingFactory, IDisposable
+    {
+        private readonly HttpClient _httpClient;
+        private bool disposedValue;
+
+        public IForexService Forex { get; }
+
+        public AlphaVantageServiceFactory()
+        {
+            _httpClient = new HttpClient();
+            var service = new AlphaVantageService(_httpClient, AppSettings, CacheService);
+            Forex = service;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _httpClient.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        ~AlphaVantageServiceFactory()
+        {
+             Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ tests/Kubera.App.IntegrationTests/IntegrationTestingFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Bogus;
+using Kubera.General;
+using Kubera.General.Services;
+using Moq;
+
+namespace Kubera.App.IntegrationTests
+{
+    public abstract class IntegrationTestingFactory
+    {
+        public virtual IAppSettings AppSettings { get; }
+
+        public virtual ICacheService CacheService { get; }
+
+        public IntegrationTestingFactory()
+        {
+            var appMock = new Mock<IAppSettings>();
+            var cacheMock = new Mock<ICacheService>();
+
+            appMock.SetupGet(a => a.AlphaVantageApiKey)
+                .Returns(Environment.GetEnvironmentVariable("ALPHA_VANTAGE_API"));
+
+            cacheMock.Setup(c => c.Get<It.IsAnyType>(It.IsAny<object[]>()))
+                .Returns(() => default);
+            cacheMock.Setup(c => c.Add(It.IsAny<It.IsAnyType>(), It.IsAny<object[]>()));
+            cacheMock.Setup(c => c.Remove<It.IsAnyType>(It.IsAny<object[]>()));
+            cacheMock.Setup(c => c.RemoveAll<It.IsAnyType>());
+            cacheMock.Setup(c => c.Clear());
+
+            AppSettings = appMock.Object;
+            CacheService = cacheMock.Object;
+        }
+
+        public virtual T CreateFake<T>(Action<Faker<T>> options = null)
+            where T : class
+        {
+            var faker = new Faker<T>();
+
+            options?.Invoke(faker);
+
+            return faker.Generate();
+        }
+    }
+}
