e31a228d15856d78ea7314091ce9f5d5dac5ea2e
Ababei Andrei <AbabeiAndrei@outlook.com>
2021-03-20 23:36:29 +0200

    Release App: Alpha.2.0.5/Api: Alpha.1.0.2

--- src/Kubera.App/Static/Resources.cs
+++ src/Kubera.App/Static/Resources.cs
@@ -4,7 +4,7 @@
     {
         public const string AppName = "Loot";
         public const string ApiName = "Kubera";
-        public const string AppVersion = "Alpha.2.0.4";
-        public const string ApiVersion = "Alpha.1.0.1";
+        public const string AppVersion = "Alpha.2.0.5";
+        public const string ApiVersion = "Alpha.1.0.2";
     }
 }
