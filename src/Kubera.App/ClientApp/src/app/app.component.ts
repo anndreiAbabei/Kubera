9a82dc65da6fca75cb69d3b7e406d365477e9b44
Ababei Andrei <AbabeiAndrei@outlook.com>
2021-03-21 15:04:11 +0200

    Make layout

--- src/Kubera.App/ClientApp/src/app/app.component.ts
+++ src/Kubera.App/ClientApp/src/app/app.component.ts
@@ -3,7 +3,8 @@ import { ThemeService } from 'src/services/theme.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
